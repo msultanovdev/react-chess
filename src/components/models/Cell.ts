50008156ce99ee245568d11cb80c977b639e64ce
msultanovdev <mukhammadamin.sultanov@gmail.com>
2022-05-29 01:28:13 +0500

    Created Chess Board

---
+++ src/components/models/Cell.ts
@@ -0,0 +1,23 @@
+import { Board } from "./Board";
+import { Colors } from "./Colors";
+import { Figure } from "./figures/Figure";
+
+export class Cell {
+    readonly x: number;
+    readonly y: number;
+    readonly color: Colors;
+    figure: Figure | null;
+    board: Board;
+    available: boolean;
+    id: number;
+
+    constructor(boadr: Board, x: number, y: number, color: Colors, figure: Figure | null) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.figure = figure;
+        this.board = boadr;
+        this.available = false;
+        this.id = Math.random();
+    }
+}
